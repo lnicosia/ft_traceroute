58660cc67004829f937d1e658889f797e7fcf542
lnicosia <lnicosia@student.42.fr>
2022-07-25 17:57:55 +0200

    [ADD] Reaching target seems ok. Need to handle printing correctly now.

--- src/receive_messages.c
+++ src/receive_messages.c
@@ -12,7 +12,9 @@ void	print_probes(uint8_t ttl, t_env *env)
 	{
 		if (env->probes[i].ttl == ttl)
 		{
+			//printf("ttl = %hhu\n", ttl);
 			probe = &env->probes[i];
+			//printf("probe received = %d\n", probe->received);
 			if (probe->received == 1)
 			{
 				if (first == 0)
@@ -35,7 +37,10 @@ void	print_probes(uint8_t ttl, t_env *env)
 		}
 	}
 	if (env->dest_reached == 0)
-		env->last_printed_ttl = ttl;
+	{
+		env->last_printed_ttl = (uint8_t)(ttl - 1);
+		//printf("\nlast printed updated to %hhu\n", ttl - 1);
+	}
 }
 
 void	update_probes(ssize_t recv_bytes, struct sockaddr_in recv_addr,
@@ -57,7 +62,6 @@ void	update_probes(ssize_t recv_bytes, struct sockaddr_in recv_addr,
 				env->probes[i].recv_bytes = recv_bytes;
 				env->probes[i].recv_addr = recv_addr;
 				env->probes[i].recv_time = get_time();
-				//close(env->udp_sockets[i]);
 				received++;
 				found = 1;
 			}
@@ -65,11 +69,11 @@ void	update_probes(ssize_t recv_bytes, struct sockaddr_in recv_addr,
 				received++;
 		}
 	}
-	/*for (i = 0; i < env->squeries * 2; i++)
+	for (i = 0; i < env->squeries * 2; i++)
 	{
-		printf("probe %ld ttl = %d, received = %d used = %d\n",
-			i, env->probes[i].ttl - 1, env->probes[i].received, env->probes[i].used);
-	}*/
+		//printf("probe %ld ttl = %d, received = %d used = %d\n",
+		//	i, env->probes[i].ttl - 1, env->probes[i].received, env->probes[i].used);
+	}
 	//printf("received %d ttl %d\n", received, ttl);
 	//	We received all the sent probes for this ttl
 	if (received == env->probes_per_hop)
@@ -97,7 +101,7 @@ void	flush_received_packets(t_env *env)
 	{
 		printf("%2ld  ", env->curr_hop++ + 1);
 		printf("(%hhu) ", i);
-		print_probes(i, env);
+		print_probes((uint8_t)(i + 1), env);
 		printf("\n");
 	}
 }
@@ -130,6 +134,8 @@ void	receive_messages(t_probe *probe, t_env *env)
 			//printf("*");
 		}
 		env->outgoing_packets--;
+		//if (env->last_ttl != 0)
+
 	}
 	else
 	{
@@ -149,7 +155,6 @@ void	receive_messages(t_probe *probe, t_env *env)
 			print_icmp_header(icmphdr);
 		}
 		//printf("ttl = %d\n", ip->ip_ttl);
-		//analyze_probe(probe, env);
 		update_probes(recv_bytes, recv_addr, ip->ip_ttl, env);
 		if (env->dest_ip.sin_addr.s_addr == ip->ip_src.s_addr
 			&& env->last_ttl == 0
@@ -157,11 +162,10 @@ void	receive_messages(t_probe *probe, t_env *env)
 			|| (env->opt & OPT_MODE_UDP && icmphdr->type == ICMP_DEST_UNREACH
 				&& icmphdr->code == ICMP_PORT_UNREACH)))
 		{
-			//printf("Reached\nDest ip = %s\nCurr ip = %s\n",
-			//	inet_ntoa(env->dest_ip.sin_addr),
-			//	inet_ntoa(env->current_gateway.sin_addr));
-			printf("Reached at ttl = %hhu\n", ip->ip_ttl);
+			//printf("Reached at ttl = %hhu\n", ip->ip_ttl);
 			env->last_ttl = ip->ip_ttl;
+			env->dest_reached = 1;
+			//printf("Last printed ttl = %hhu\n", env->last_printed_ttl);
 		}
 	}
 }
