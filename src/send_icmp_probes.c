19165326d421320b5d6105f060ddb4817cfbacf6
lnicosia <lnicosia@student.42.fr>
2022-08-02 22:13:03 +0200

    [ADD] icmp probes cleanup.

--- src/send_icmp_probes.c
+++ src/send_icmp_probes.c
@@ -16,7 +16,6 @@ void	fill_icmp_header(struct icmphdr* icmphdr, t_env *env)
 
 static void	send_current_probes(t_env *env)
 {
-	//dprintf(STDOUT_FILENO, "Sending on socket %ld\n", env->total_sent);
 	if (env->opt & OPT_VERBOSE)
 		dprintf(STDOUT_FILENO, "Sending ttl=%hhd sequence=%d\n",
 			env->ttl, env->port);
@@ -28,8 +27,6 @@ static void	send_current_probes(t_env *env)
 	env->probes[env->total_sent].probe = env->curr_probe;
 	env->probes[env->total_sent].checksum = ((struct udphdr*)env->out_buff)->uh_sum;
 	env->probes[env->total_sent].port = htons(env->port);
-	//env->dest_ip.sin_port = htons(env->port++);
-	//env->icmp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (setsockopt(env->icmp_socket, SOL_IP, IP_TTL,
 		&env->ttl, sizeof(env->ttl)))
 	{
@@ -43,7 +40,6 @@ static void	send_current_probes(t_env *env)
 		free_and_exit_failure(env);
 	}
 	env->probes[env->total_sent].send_time = get_time();
-	//close(env->udp_socket);
 	env->outgoing_packets++;
 	env->total_sent++;
 	env->curr_probe++;
@@ -69,22 +65,11 @@ int		send_icmp_probes(t_env *env)
 			&& env->total_sent < env->max_packets
 			&& !are_last_ttl_probes_all_sent(env))
 		{
-			//dprintf(STDOUT_FILENO, "Sending\n");
 			send_current_probes(env);
 		}
-		else// if (env->total_received < env->max_packets)
-		{
-			//dprintf(STDOUT_FILENO, "Receiving\n");
-			//dprintf(STDOUT_FILENO, "%ld used probes\n", env->used_probes);
-			/*dprintf(STDOUT_FILENO, "%ld outgoing packets\n", env->outgoing_packets);
-			dprintf(STDOUT_FILENO, "%ld used probes\n", env->used_probes);
-			dprintf(STDOUT_FILENO, "%ld/%ld sent\n",
-				env->total_sent, env->max_packets);
-			dprintf(STDOUT_FILENO, "%d\n", are_last_ttl_probes_all_sent(env));*/
+		else
 			receive_messages(env);
-		}
 	}
-	//dprintf(STDOUT_FILENO, "End of loop\n");
 	if (env->last_ttl == 0)
 		flush_received_packets(env->ttl, env);
 	else
